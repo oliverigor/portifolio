7c5a4b4ac018164f274ef2e28a1e8f1784d9da0a
Igor Martins <m.oliverunb@gmail.com>
2018-04-25 17:06:11 -0300

    adicionando svg das competencias

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import Messenger from './messenger.js'
 import Photo from './photo.png'
+import Skills from './skills.svg'
+import Computer from './computer.svg'
+import Job from './job.svg'
+import Letter from './letter.svg'
+import Like from './like.svg'
 
 class App extends Component {
   render() {
@@ -9,13 +14,31 @@ class App extends Component {
       <div className="App">
 
           <div className="App-Louco">
-          <img src={Photo} width={200}/>
+          <img className="self-photo" src={Photo} width={200}/>
           <h1 className="App-title">Igor Martins</h1>
           </div>
 
         <p className="App-intro">
         <Messenger news={['Olá', 'Me chamo igor', 'Era engenheiro automotivo', 'Porém agora sou aspirante a desenvolvedor Full-stack']}/>
         </p>
+
+        <div className="skillset">
+          <div>
+            <img src={Skills} width={100} />
+          </div>
+          <div>
+            <img src={Computer} width={100} />
+          </div>
+          <div>
+            <img src={Job} width={100} />
+          </div>
+          <div>
+            <img src={Like} width={100} />
+          </div>
+          <div>
+            <img src={Letter} width={100} />
+          </div>
+        </div>
       </div>
     );
   }
