7dbd7c1d67910ed275b7fb2995c14db2196f8980
Igor Martins <m.oliverunb@gmail.com>
2018-04-27 10:35:13 -0300

    textos alinhados, porem com erro na mensagem

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Computer from './computer.svg'
 import Job from './job.svg'
 import Letter from './letter.svg'
 import Like from './like.svg'
+import Current from './curriculum.svg'
 
 
 
@@ -18,7 +19,7 @@ class App extends Component {
 
 
         <div className="App-Louco">
-          <img className="self-photo" src={Photo} width={200}/>
+          <img className="self-photo" src={Photo} width={150}/>
           <h1 className="App-title">Igor Martins</h1>
         </div>
 
@@ -32,22 +33,49 @@ class App extends Component {
         <div className="skill_set">
 
           <div className="skill_box">
-            <img src={Skills} width={100} />
-            <h3 className="text_box">lorem ipsum eh meu bigulao eh nois porra ehuashauehasu, valeu sociedade capitalista, odeio capital inicial e é nois</h3>
+            <img src={Computer} width={100} />
+            <div>
+              <h2 className="title">PROJETOS</h2>
+              <h3 className="text_box">lorem ipsum eh meu bigulao eh nois porra ehuashauehasu, valeu sociedade capitalista, odeio capital inicial e é nois</h3>
+            </div>
           </div>
           <div className="skill_box">
-            <img src={Computer} width={100} />
+            <img src={Skills} width={100} />
+              <div>
+                <h2 className="title">HABILIDADES</h2>
+                <h3 className="text_box">lorem ipsum eh meu bigulao eh nois porra ehuashauehasu, valeu sociedade capitalista, odeio capital inicial e é nois</h3>
+              </div>
+          </div>
+          <div className="skill_box">
+            <img src={Current} width={100} />
+              <div>
+                <h2 className="title">TRABALHO ATUAL</h2>
+                <h3 className="text_box">lorem ipsum eh meu bigulao eh nois porra ehuashauehasu, valeu sociedade capitalista, odeio capital inicial e é nois</h3>
+              </div>
           </div>
           <div className="skill_box">
             <img src={Like} width={100} />
+              <div>
+                <h2 className="title">O QUE EU CURTO</h2>
+                <h3 className="text_box">lorem ipsum eh meu bigulao eh nois porra ehuashauehasu, valeu sociedade capitalista, odeio capital inicial e é nois</h3>
+              </div>
           </div>
           <div className="skill_box">
             <img src={Job} width={100} />
+              <div>
+                <h2 className="title">ME CONTRATE</h2>
+                <h3 className="text_box">lorem ipsum eh meu bigulao eh nois porra ehuashauehasu, valeu sociedade capitalista, odeio capital inicial e é nois</h3>
+              </div>
           </div>
           <div className="skill_box">
             <div>
             <img src={Letter} width={100} />
+            <div>
+              <h2 className="title">DEIXE SUA MENSAGEM</h2>
+              <h3 className="text_box">lorem ipsum eh meu bigulao eh nois porra ehuashauehasu, valeu sociedade capitalista, odeio capital inicial e é nois</h3>
             </div>
+            </div>
+
             <div style={{marginTop:'50px'}}>
             <form action="https://formspree.io/m.oliverunb@gmail.com" method="POST">
 
