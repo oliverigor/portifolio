db0c2e0ec4c1a1260944980c135539230bdb08bf
Igor de Oliveira Martins <oliverigor@Igors-MacBook-Pro.local>
2021-12-26 17:07:28 -0500

    Reorganizing a bit

---
+++ src/components/Footer.js
@@ -0,0 +1,57 @@
+import React from "react"
+import "../App.css"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="social-btns">
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          className="btn facebook"
+          href="https://www.facebook.com/oliveigor"
+        >
+          <i className="fa fa-facebook" />
+        </a>
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          className="btn twitter"
+          href="https://twitter.com/oliverig0r"
+        >
+          <i className="fa fa-twitter" />
+        </a>
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          className="btn google"
+          href="https://mail.google.com/mail/?view=cm&fs=1&to=m.oliverunb@gmail.com"
+        >
+          <i className="fa fa-google" />
+        </a>
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          className="btn github"
+          href="https://github.com/oliverigor"
+        >
+          <i className="fa fa-github" />
+        </a>
+      </div>
+      <small>
+        <p>
+          MADE BY ME...YES...
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://github.com/oliverigor"
+          >
+            IGOR
+          </a>
+        </p>
+      </small>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/images/index.js
@@ -0,0 +1,8 @@
+export { default as Computer } from "./computer.svg"
+export { default as Skills } from "./skills.svg"
+export { default as Job } from "./job.svg"
+export { default as Letter } from "./letter.svg"
+export { default as Like } from "./like.svg"
+export { default as Current } from "./curriculum.svg"
+export { default as Myself } from "./igorphoto.png"
+export { default as Projects } from "./project.svg"
