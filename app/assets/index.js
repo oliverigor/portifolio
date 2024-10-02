28b7e7f0d7ec9e5316337f1fd84c7a103b376abd
Igor Martins <igor.deoliveiramartins@shopify.com>
2024-10-02 15:38:02 -0400

    adding filter

---
+++ app/assets/index.js
@@ -0,0 +1,5 @@
+export { default as Myself } from "./igorphoto.png"
+export { default as github } from "./github.svg"
+export { default as twitter } from "./twitter.svg"
+export { default as google } from "./google.svg"
+export { default as linkedin } from "./linkedin.svg"
